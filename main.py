0cac061123e0f0092422c8ae879ad8ce26bd933c
RobotDaniel <101746899+RobotDaniel@users.noreply.github.com>
2026-03-08 19:41:23 +0000

    added error handling

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import csv
 import random
-county=input("Enter a County")
-print("county")
+
+def Menu():
+    menuInput=input("1:Station Pick")
+    if menuInput=="1":
+        data_list=csvRead()
+        StationPick(data_list)
+    else:
+        print("invalid option")
+        Menu()
 
 def csvRead(): 
+    print("csv")
     with open('NATIONAL_RAIL_STATION_LOG_-_FEB_2025_update.csv', mode='r') as file:
         csv_reader = csv.DictReader(file)  # Create DictReader
 
@@ -12,19 +20,38 @@ def csvRead():
             data_list.append(row)
         return data_list
         
-def StationPick(): 
-    stations=[]
+def StationPick(data_list): 
+    county=input("Enter a County")
+    print("county")
+    global stationsPickList
+    stationsPickList=[]
     for data in data_list:
-    if data['County']==county and data['Visited']=="No":
-        stations.append(data['Station'])
-    print(stations)
-    numberOfStations=input("how many staions do you want to vist")
-
-    for i in range(int(numberOfStations)):
+        if data['County']==county and data['Visited']=="No":
+            stationsPickList.append(data['Station'])
+    print(stationsPickList)
     
-        print(stations[random.randint(0,(len(stations)-1))])
-
-
+    stationRandPick()
+def stationRandPick():
+    global stationsPickList
+    numberOfStations=input("5how many staions do you want to vist")
+    try:
+        numberOfStations=int(numberOfStations)
+    except:
+        print("not a number")
+        stationRandPick(55)
+    if numberOfStations>len(stationsPickList):
+        print("too many stations")
+        stationRandPick()
+    else:    
+        for i in range(int(numberOfStations)):
+        
+            num=random.randint(0,(len(stationsPickList)-1))
+            station=stationsPickList[num]
+            #print(num)
+            print(station)
+            stationsPickList.remove(station)
+            menuInput=input("continue\n Y=Yes\nN=No (return to menu)")
+Menu()
 
         
 
