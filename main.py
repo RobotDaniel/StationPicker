d636c9f81cd52e8cfbd16ee324235f4c8eb0376b
RobotDaniel <101746899+RobotDaniel@users.noreply.github.com>
2026-03-08 21:51:44 +0000

    added column retrieval function

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ import csv
 import random
 
 def Menu():
-    menuInput=input("1:Station Pick")
+    menuInput=input("1:Station Pick\n2:Retrive Column\n3:station infomation")
+
     if menuInput=="1":
         data_list=csvRead()
         StationPick(data_list)
+    elif menuInput=="2":
+        data_list=csvRead()
+        RetriveColumns(data_list)
+    elif menuInput=="3"
+        data_list=csvRead()
+
     else:
         print("invalid option")
         Menu()
@@ -19,7 +26,29 @@ def csvRead():
         for row in csv_reader:
             data_list.append(row)
         return data_list
-        
+def RetriveColumns(data_list):
+    itemSelect=input("enter column you want data from")
+    itemList = []
+    for data in data_list:
+        if any(x == data[itemSelect] for x in itemList)==False:
+            itemList.append(data[itemSelect])
+            print(data[itemSelect])          
+    #print(itemList)
+    menuInput=input("continue\nY=yes \nN=no (return to menu)")
+    if menuInput.lower=="Y":
+        RetriveColumns()
+    else:
+        Menu()
+def stationInfo(data_list):
+    stationName=input("enter a station to retrive info")
+    for data in data_list:
+        if data["Station"]==stationName:
+            print("")
+
+
+
+
+
 def StationPick(data_list): 
     county=input("Enter a County")
     print("county")
